45cc7fe56377e1499614d869790b1f29db40127c
lseongmi <w2307@e-mirim.hs.kr>
2025-04-08 00:43:35 +0900

    Feat : nav바 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './route/login/Login';
 import Signup from './route/signup/Signup';
+import Nav from "./components/nav";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path='/login' element={<Login />}></Route>
                 <Route path='/signup' element={<Signup />}></Route>
+                <Route path='/nav' element={<Nav />}></Route>
             </Routes>
         </Router>
     );
