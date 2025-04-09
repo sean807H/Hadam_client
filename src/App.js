6ebd9013041fc69fa95cdf41af4213bbb3e3be82
sean807H <ksyssyy070814@gmail.com>
2025-04-09 19:50:43 +0900

    Feat : 감사일기&후회일기 페이지 개발

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Login from './route/login/Login';
 import Signup from './route/signup/Signup';
 import Nav from "./components/nav";
+import Write from "./components/Write";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Route path='/login' element={<Login />}></Route>
                 <Route path='/signup' element={<Signup />}></Route>
                 <Route path='/nav' element={<Nav />}></Route>
+                <Route path='/Write' element={<Write />}></Route>
             </Routes>
         </Router>
     );
